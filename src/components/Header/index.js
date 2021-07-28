da25ba26e14bb30a76052afbc1689ca85f0690e9
Matt Liu <mattliuym@gmail.com>
2021-07-28 15:34:35 +1200

    Added the tab bar and modified some css files.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,8 @@
-import React,{Component} from 'react'
-import header from './index.module.css'
+import React,{Component} from 'react';
+import header from './index.module.css';
 
 export default class Header extends Component{
     render() {
-        return <header><h1 className={header.Myheader}>Waikato Car Park</h1></header>//this name is customized which will be read by ajax in the future.
+        return <header><h1 className={header.myHeader}>Waikato Car Park</h1></header>//this name is customized which will be read by ajax in the future.
     }
-}
+};
