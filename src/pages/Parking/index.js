138f38fc2d8546633ee811ee4ed2209baba5af8b
Matt Liu <mattliuym@gmail.com>
2021-08-12 00:45:45 +1200

    Add the axios methods

--- src/pages/Parking/index.js
+++ src/pages/Parking/index.js
@@ -1,12 +1,13 @@
 import React,{Component} from "react";
 import './index.css';
 import Parkimg from './images.jpeg'
-
+import axios from 'axios'
 import { SearchBar, Button, WhiteSpace, WingBlank } from 'antd-mobile';
 
 export default class Parking extends Component {
     state = {
         value: '',
+        result:'',
     };
     componentDidMount() {
         //this.autoFocusInst.focus();
@@ -20,16 +21,49 @@ export default class Parking extends Component {
     handleClick = () => {
         //this.manualFocusInst.focus();
     }
+    //get the car information from backend
     getPlate = () =>{
         const {searchInput} = this;
-        //console.log(searchInput.state.value);
         this.searchPlate(searchInput.state.value);
     }
-    //this method is for searching plate by Ajax from the database
+    //reformat the date format into a readable style.
+    getDate=(time)=>{
+        let myDate = new Date(time);
+        let year = myDate.getFullYear();
+        let month = (myDate.getMonth()+1 < 10 ? '0'+(myDate.getMonth()+1) : myDate.getMonth()+1) ;//获取月
+        let date = myDate.getDate();
+        let  h = myDate.getHours();
+        let m = myDate.getMinutes();
+        let now = `${date}/${month}/${year} at ${h}:${m}`;
+        return now
+    }
+    //this method is for searching plate by Axios from the database
     searchPlate=(plate)=>{
-        console.log(plate);
+        //console.log(plate);
+        axios.get('/api/SearchPlate/GetCarInfo',{params:{s:plate}}).then(
+            response=>{
+                if(!response.data[0])
+                    return;
+                this.setState({result:response.data[0]});
+            },
+            error=>{
+                console.log(error);
+            }
+        )
     }
     render() {
+        let plateinfo;
+        if(this.state.result!=""){
+            plateinfo=
+                <div id={'plateInfo'}>
+                    <table>
+                        <tbody>
+                            <tr><td>Plate Number: </td><td>{this.state.result.plate}</td></tr>
+                            <tr><td>Entered Time: </td><td>{this.getDate(this.state.result.inTime)}</td></tr>
+                        </tbody>
+                    </table>
+                </div>;
+        }
         return (<div>
             {/*<WingBlank><div className="sub-title">Normal</div></WingBlank>*/}
             <div className={'imgBox'}>
@@ -38,7 +72,7 @@ export default class Parking extends Component {
             <SearchBar placeholder="Please enter your plate"
                        maxLength={6}
                        cancelText={"Cancel"}
-                       onSubmit={value=>this.searchPlate(value)}
+                       onSubmit={value=>this.searchPlate(value.toUpperCase())}
                        ref={c=>this.searchInput=c}
             />
             <WhiteSpace />
@@ -48,34 +82,7 @@ export default class Parking extends Component {
                 >Click to Search</Button>
             </WingBlank>
             <WhiteSpace />
-            {/*start from here*/}
-            {/*<WingBlank><div className="sub-title">AutoFocus when enter page</div></WingBlank>*/}
-            {/*<SearchBar placeholder="自动获取光标" ref={ref => this.autoFocusInst = ref} />*/}
-            {/*<WhiteSpace />*/}
-            {/*<WingBlank><div className="sub-title">Focus by operation</div></WingBlank>*/}
-            {/*<SearchBar*/}
-            {/*    placeholder="手动获取获取光标"*/}
-            {/*    ref={ref => this.manualFocusInst = ref}*/}
-            {/*/>*/}
-            {/*<WhiteSpace />*/}
-            {/*<WingBlank>*/}
-            {/*    <Button*/}
-            {/*        onClick={this.handleClick}*/}
-            {/*    >click to focus</Button>*/}
-            {/*</WingBlank>*/}
-            {/*<WhiteSpace />*/}
-            {/*<WingBlank><div className="sub-title">Show cancel button</div></WingBlank>*/}
-            {/*<SearchBar*/}
-            {/*    value={this.state.value}*/}
-            {/*    placeholder="Search"*/}
-            {/*    onSubmit={value => console.log(value, 'onSubmit')}*/}
-            {/*    onClear={value => console.log(value, 'onClear')}*/}
-            {/*    onFocus={() => console.log('onFocus')}*/}
-            {/*    onBlur={() => console.log('onBlur')}*/}
-            {/*    onCancel={() => console.log('onCancel')}*/}
-            {/*    showCancelButton*/}
-            {/*    onChange={this.onChange}*/}
-            {/*/>*/}
+            {plateinfo}
         </div>);
     }
 }
