d0d57bf3b41ca07dd71089a54f7d3cab3c706448
Matt Liu <mattliuym@gmail.com>
2021-08-31 23:46:44 +1200

    Some updates;

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
